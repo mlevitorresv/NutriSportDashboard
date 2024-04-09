eb47b1849c7d10f37d60aaf5a9540c26e662e827
mlevitorresv <levitorres66@gmail.com>
2024-04-09 15:11:21 +0200

    NAME CHANGED ON dASHBOARDpAGE

---
+++ src/pages/home/DashboardPage.tsx
@@ -0,0 +1,15 @@
+import { DivKPI } from '../../components/KPI/DivKPI'
+import { MessageDivStyled } from '../../components/messagesContact/MessagesDivStyled'
+import { MessagesContainer } from '../../components/messagesContact/MessagesContainer'
+
+export const Dashboard = () => {
+    return (
+        <>
+            <DivKPI />
+            <MessageDivStyled>
+                <h1 className='messages__title'>Latest review by customers</h1>
+                <MessagesContainer />
+            </MessageDivStyled>
+        </>
+    )
+}
