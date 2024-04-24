b5f0696f48ca0d55d7953cce5f1e4e1d93c59e73
mlevitorresv <levitorres66@gmail.com>
2024-04-24 12:02:19 +0200

    AsideNav with all elements

--- src/components/header/AsideNav.tsx
+++ src/components/header/AsideNav.tsx
@@ -9,43 +9,62 @@ import { FaPhoneAlt } from "react-icons/fa";
 import { DivStyled } from '../common/DivStyled'
 import { ButtonStyled } from '../common/ButtonStyled';
 import { AsideNavPropsInterface } from '../../interfaces/componentsInterface';
+import { FaMoneyBills } from "react-icons/fa6";
+import { GrUserAdmin } from "react-icons/gr";
+import { GiMasonJar } from "react-icons/gi";
+import { MdOutlinePointOfSale } from "react-icons/md";
+import { FaTruckMoving } from "react-icons/fa";
+import { FaCommentAlt } from "react-icons/fa";
+
+
 
 
 export const AsideNav = (props: AsideNavPropsInterface) => {
-  return (
-    <AsideNavStyled>
-        <img src="../../../public/logoDashboard.png" alt="hotel miranda's logo" />
-        <div className='icons'>
-            <NavLinkStyled to="/home">
-                <LuLayoutDashboard />
-                Dashboard
-            </NavLinkStyled>
-            <NavLinkStyled to="/rooms">
-                <SlKey />
-                Rooms
-            </NavLinkStyled>
-            <NavLinkStyled to="/bookings">
-                <LuCalendarCheck2 />
-                Bookings
-            </NavLinkStyled>
-            <NavLinkStyled to="/users">
-                <FiUsers />
-                Users
-            </NavLinkStyled>
-            <NavLinkStyled to="/contact">
-                <FaPhoneAlt />
-                Contact
-            </NavLinkStyled>
-        </div>
-        <DivStyled>
-            <img src="../../public/levi.jpeg" alt="user photo" />
-            <p className='name'>Manuel Leví</p>
-            <p className='email'>levitorres66@gmail.com</p>
-            <ButtonStyled>Edit</ButtonStyled>
-        </DivStyled>
-        <p className='desc'>Travl Hotel Admin Dashboard</p>
-        <p className='green'>&copy; 2020 All Rights Reserved</p>
-        <p className='green'>Made with ♥ by Manuel Leví</p>
-    </AsideNavStyled>
-  )
+    return (
+        <AsideNavStyled>
+            <img src="src\assets\logo.JPG" alt="NutriSport's logo" />
+            <div className='icons'>
+                <NavLinkStyled to="/home">
+                    <LuLayoutDashboard />
+                    Dashboard
+                </NavLinkStyled>
+                <NavLinkStyled to="/bills">
+                    <FaMoneyBills />
+                    Bills
+                </NavLinkStyled>
+                <NavLinkStyled to="/customers">
+                    <FiUsers />
+                    Customers
+                </NavLinkStyled>
+                <NavLinkStyled to="/employees">
+                    <GrUserAdmin />
+                    Employees
+                </NavLinkStyled>
+                <NavLinkStyled to="/products">
+                    <GiMasonJar />
+                    Products
+                </NavLinkStyled>
+                <NavLinkStyled to="/sales">
+                    <MdOutlinePointOfSale />
+                    Sales
+                </NavLinkStyled>
+                <NavLinkStyled to="/suppliers">
+                    <FaTruckMoving />
+                    Suppliers
+                </NavLinkStyled>
+                <NavLinkStyled to="/comments">
+                    <FaCommentAlt  />
+                    Comments
+                </NavLinkStyled>
+            </div>
+            <DivStyled>
+                <p className='name'>Manuel Leví</p>
+                <p className='email'>levitorres66@gmail.com</p>
+                <ButtonStyled>Edit</ButtonStyled>
+            </DivStyled>
+            <p className='desc'>NutriSport Admin Dashboard</p>
+            <p className='green'>&copy; 2024 All Rights Reserved</p>
+            <p className='green'>Made with ♥ by Manuel Leví</p>
+        </AsideNavStyled>
+    )
 }
