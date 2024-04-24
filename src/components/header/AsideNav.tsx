ef46264040ab11c14f04c728368682e51c4bcd37
mlevitorresv <levitorres66@gmail.com>
2024-04-24 15:05:56 +0200

    AsideNav with info of user

--- src/components/header/AsideNav.tsx
+++ src/components/header/AsideNav.tsx
@@ -1,11 +1,7 @@
-import React from 'react'
 import { AsideNavStyled } from './AsideNavStyled'
 import { NavLinkStyled } from './NavLinkStyled.js'
 import { LuLayoutDashboard } from "react-icons/lu";
-import { SlKey } from "react-icons/sl";
-import { LuCalendarCheck2 } from 'react-icons/lu';
 import { FiUsers } from "react-icons/fi";
-import { FaPhoneAlt } from "react-icons/fa";
 import { DivStyled } from '../common/DivStyled'
 import { ButtonStyled } from '../common/ButtonStyled';
 import { AsideNavPropsInterface } from '../../interfaces/componentsInterface';
@@ -15,11 +11,14 @@ import { GiMasonJar } from "react-icons/gi";
 import { MdOutlinePointOfSale } from "react-icons/md";
 import { FaTruckMoving } from "react-icons/fa";
 import { FaCommentAlt } from "react-icons/fa";
+import { useAuth } from '../../context/AuthProvider.js';
+
 
 
 
 
 export const AsideNav = (props: AsideNavPropsInterface) => {
+    const { user } = useAuth();
     return (
         <AsideNavStyled>
             <img src="src\assets\logo.JPG" alt="NutriSport's logo" />
@@ -58,13 +57,9 @@ export const AsideNav = (props: AsideNavPropsInterface) => {
                 </NavLinkStyled>
             </div>
             <DivStyled>
-                <p className='name'>Manuel Leví</p>
-                <p className='email'>levitorres66@gmail.com</p>
+                <p className='email'>{ user?.email} </p>
                 <ButtonStyled>Edit</ButtonStyled>
             </DivStyled>
-            <p className='desc'>NutriSport Admin Dashboard</p>
-            <p className='green'>&copy; 2024 All Rights Reserved</p>
-            <p className='green'>Made with ♥ by Manuel Leví</p>
         </AsideNavStyled>
     )
 }
