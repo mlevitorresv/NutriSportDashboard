c1607d4b65a7530d1f43f85ce7c9fd3d17c90ab2
mlevitorresv <levitorres66@gmail.com>
2024-04-25 10:28:35 +0200

    all slices added to store

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,12 +2,24 @@ import { configureStore } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import { commentSlice } from "../features/comments/commentsSlice";
+import { billSlice } from "../features/bills/billsSlice";
+import { customerSlice } from "../features/customers/customersSlice";
+import { employeeSlice } from "../features/employees/employeesSlice";
+import { productSlice } from "../features/products/productsSlice";
+import { saleSlice } from "../features/sales/salesSlice";
+import { supplierSlice } from "../features/suppliers/suppliersSlice";
 
 
 
 export const store = configureStore({
     reducer: {
-        comment: commentSlice.reducer
+        comment: commentSlice.reducer,
+        bills: billSlice.reducer,
+        customer: customerSlice.reducer,
+        employee: employeeSlice.reducer,
+        product: productSlice.reducer,
+        sale: saleSlice.reducer,
+        supplier: supplierSlice.reducer,
     }
 })
 
