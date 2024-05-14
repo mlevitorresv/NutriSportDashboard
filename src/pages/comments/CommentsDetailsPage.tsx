431eed82e3705531e2e4ebc7e64531490e44223e
mlevitorresv <levitorres66@gmail.com>
2024-05-14 15:38:34 +0200

    RM: comments, sales and suppliers pages of details

--- src/pages/comments/CommentsDetailsPage.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const CommentsDetailsPage = () => {
-    return (
-        <div>
-
-        </div>
-    )
-}
--- src/pages/suppliers/SuppliersDetailsPage.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const SuppliersDetailsPage = () => {
-    return (
-        <div>
-
-        </div>
-    )
-}
